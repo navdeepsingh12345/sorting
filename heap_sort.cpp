a15c489297aa1444fd9ffe69c09546bed888af7f
navdeepsingh12345 <115405230+navdeepsingh12345@users.noreply.github.com>
2023-04-10 18:05:07 +0530

    Update heap_sort.cpp

--- heap_sort.cpp
+++ heap_sort.cpp
@@ -1,12 +1,10 @@
 
 #include <stdio.h>
-void Insert(int A[], int n)
-{
+void Insert(int A[], int n){
     int i = n, temp;
     temp = A[i];
 
-    while (i > 1 && temp > A[i / 2])
-    {
+    while (i > 1 && temp > A[i / 2]){
         A[i] = A[i / 2];
         i = i / 2;
     }
@@ -23,12 +21,10 @@ int Delete(int A[], int n)
     A[n] = val;
     i = 1;
     j = i * 2;
-    while (j <= n - 1)
-    {
+    while (j <= n - 1){
         if (j < n - 1 && A[j + 1] > A[j])
             j = j + 1;
-        if (A[i] < A[j])
-        {
+        if (A[i] < A[j]){
             temp = A[i];
             A[i] = A[j];
             A[j] = temp;
